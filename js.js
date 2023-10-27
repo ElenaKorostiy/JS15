2df70502785532db6a8f5d3deaafc72ac0de85eb
ElenaKorostiy <go.lena.83@gmail.com>
2023-10-27 22:27:27 +0300

    dz31

--- js.js
+++ js.js
@@ -1,2 +1,21 @@
-const tasks = ["Завдання 1", "Завдання 2", "Завдання 3"];
-localStorage.setItem("tasks", JSON.stringify(tasks));
+
+const tasks = JSON.parse(localStorage.getItem("tasks")) || ["Завдання 1", "Завдання 2", "Завдання 3"];
+
+
+function updateTaskList() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+    for (const task of tasks) {
+        const li = document.createElement("li");
+        li.textContent = task;
+        taskList.append(li);
+    }
+}
+
+// Запуск функції оновлення списку при завантаженні сторінки
+window.addEventListener("load", updateTaskList);
+
+// Заборона видалення завдань
+window.addEventListener("beforeunload", function (event) {
+    event.returnValue = "Ви впевнені, що хочете залишити цю сторінку?";
+});
